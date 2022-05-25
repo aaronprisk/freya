5be30a59712717cbecb361096a29b2442bf994b1
Aaron Prisk <aaronprisk@gmail.com>
2022-05-25 09:39:57 -0400

    Initial Upload
    First iteration of vm backup script

---
+++ vm-backup.php
@@ -0,0 +1,32 @@
+<?php
+// START SESSION
+session_start();
+
+// LOGOUT REQUEST
+if (isset($_POST["logout"])) { unset($_SESSION["user"]); }
+
+// REDIRECT TO LOGIN PAGE IF NOT LOGGED IN
+if (!isset($_SESSION["user"])) {
+  header("Location: login.php");
+  exit();
+}
+
+// INCLUDE CONNECT FILE
+include 'connect.php';
+
+// PULL VMID VARIABLE
+$vmid = $_REQUEST['vmid'];
+
+// CREATE BACKUP DIRECTORY
+mkdir("/mnt/backups/" . $vmid, 0755);
+
+// GENERATE BACKUP XML
+$domainbackup = simplexml_load_file('/opt/seidr/backup.xml');
+$domainbackup->disks->disk[0]->target->attributes()->file="/mnt/backups/" . $vmid . '/' . $vmid . '-' . date('Y-m-d') . ".qcow2";
+$domainbackup->asXML('/mnt/backups/' . $vmid . '/' . $vmid . '.xml');
+
+// START VIRSH BACKUP
+echo "Running backup...";
+$shellout = shell_exec('virsh -c qemu:///system backup-begin Omega /mnt/backups/' . $vmid . '/' . $vmid . '.xml');
+echo "<pre>$shellout</pre>";
+?>
