8d43178f55a9db34a5cb47aa291fa722afbaa16a
Aaron Prisk <aaronprisk@gmail.com>
2022-05-25 10:21:08 -0400

    Fixed virsh command

--- seidr/seidr-thumb.sh
+++ seidr/seidr-thumb.sh
@@ -2,9 +2,9 @@
 # Seidr Thumbnailer
 # Service that takes routine snapshots of running KVM domains to use as thumbnails in Freya interface
 
-for VMS in $(virsh list --name);
+for VMS in $(virsh -c qemu:///system list --name);
 do
     mkdir /var/www/html/thumbs/$VMS
-    virsh screenshot $VMS /var/www/html/thumbs/$VMS/$VMS.ppm
+    virsh -c qemu:///system screenshot $VMS /var/www/html/thumbs/$VMS/$VMS.ppm
     convert /var/www/html/thumbs/$VMS/$VMS.ppm /var/www/html/thumbs/$VMS/$VMS.png
 done
