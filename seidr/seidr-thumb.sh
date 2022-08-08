31d40f540e908c47cd3179fa00610fc3f3efa84b
Aaron Prisk <aaronprisk@gmail.com>
2022-08-07 22:12:43 -0400

    Added check for existing thumbnail directory

--- seidr/seidr-thumb.sh
+++ seidr/seidr-thumb.sh
@@ -3,8 +3,10 @@
 # Service that takes routine snapshots of running KVM domains to use as thumbnails in Freya interface
 
 for VMS in $(virsh -c qemu:///system list --name);
-do
-    mkdir /var/www/html/thumbs/$VMS
+    if [ ! -d /var/www/html/thumbs/$VMS ]
+    then
+        mkdir /var/www/html/thumbs/$VMS
+    fi
     virsh -c qemu:///system screenshot $VMS /var/www/html/thumbs/$VMS/$VMS.ppm
     convert /var/www/html/thumbs/$VMS/$VMS.ppm /var/www/html/thumbs/$VMS/$VMS.png
 done
